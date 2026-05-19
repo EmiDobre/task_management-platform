cc3375cc2187608f5713506477fc25235e674424
Emilia Iliana Dobre <emylia.iliana@gmail.com>
2026-05-19 17:35:00 +0300

    Initial infrastructure bootstrap setup

---
+++ src/main/java/com/example/task_managementplatform/TaskManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_managementplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/task_managementplatform/TaskManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_managementplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
